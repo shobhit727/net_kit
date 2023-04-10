b68d5358c2fb7b9bf15c69a7ff0bf54007bc1390
Shobhit saraogi <shobhitsaraogi@gmail.com>
2023-04-10 22:07:26 +0530

    .

--- index_manger.py
+++ index_manger.py
@@ -13,11 +13,19 @@ class set_up:
 
         self.index = {}
 
+        # seting up data file
         if os.path.exists(r"data/index.json"):
             logger.debug("index file exists")
             with open(r"data/index.json", "r") as f:
                 self.index = json.load(f)
         else:
             logger.info("index file dose not exists")
-            index_file = open("data/index.json", "x")
-            self.index = {}
+            with open("data/index.json", "x") as index_file:
+                index_file.write(str(self.index))
+
+        if os.path.exists(r"config.yml"):
+            logger.debug("config file exists")
+        else:
+            logger.info("config file is creart")
+            with open("config.yml", "x") as config_file:
+                pass
--- sokit.py
+++ sokit.py
@@ -1,14 +1,11 @@
 import logging, socket
 import logging_manger
 
-logging.basicConfig(
-    level=logging.DEBUG,
-    format="%(asctime)s - %(name)s - %(levelname)s |   > %(message)s",
-)
+logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s |   > %(message)s")
 
 logger = logging.getLogger("sokit")
 format = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s |   > %(message)s")
 logger.setLevel(logging.DEBUG)
 
 
-#       to be completed
+socket.socket()
